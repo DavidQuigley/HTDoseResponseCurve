be44bb04486e5ee6ec4fb087e5eebeed0fc5dad5
David Quigley <UCSF>
2016-07-19 11:47:31 -0700

    fixed bug in example code

--- R/plot.R
+++ R/plot.R
@@ -303,12 +303,11 @@ set_list_default = function(L, name, val){
 #' dose_4HPR = c(0.1, 0.5, 1, 2)
 #' eff_4HPR = c(0.7666, 0.5833, 0.5706, 0.4934)
 #' eff_comb = c(0.6539, 0.4919, 0.3551, 0.2341)
-#' syn = data.frame( 
-#'         treatment_1 = rep("SCH66336", 13),
-#'         conc_1 = c( dose_SCH, rep(0, 4), dose_SCH[1:4]),
-#'         treatment_2 = rep("4-HPR", 13),
-#'         conc_2 = c( rep(0, 5), dose_4HPR, dose_4HPR ),
-#'         values = c(eff_SCH, eff_4HPR, eff_comb ) )
+#' syn = data.frame( treatment_1 = rep("SCH66336", 13),
+#'                   conc_1 = c( dose_SCH, rep(0, 4), dose_SCH[1:4]),
+#'                   treatment_2 = rep("4-HPR", 13),
+#'                   conc_2 = c( rep(0, 5), dose_4HPR, dose_4HPR ),
+#'                   values = c(eff_SCH, eff_4HPR, eff_comb ) )
 #' ds_lk = create_synergy_dataset( sample_types = rep("sample_1", 13), 
 #'                                 treatments_1 = syn$treatment_1,
 #'                                 treatments_2 = syn$treatment_2,
